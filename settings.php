e04a5b41abcddea46fbe1a928fbde984d6b4bab2
alvakim <alvakim@gmail.com>
2014-10-17 13:25:31 +0000

    Динамическое меню

--- settings.php
+++ settings.php
@@ -3,6 +3,7 @@ $Settings = array(
 	"ThisFolder"=>"..", // путь к корню сайта из папки istra
 	"ContentFolder"=>"content",
 	"XsltFolder"=>"istra/xslt",
+	"JSFolder"=>"istra/js",
 	"CacheFolder"=>"cache",
 	"OverridesFolder"=>"overrides"
 );
