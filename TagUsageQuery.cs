c7f9f4baf7e2eb9e42c8ec94207b06731245588a
alvakim <alvakim@gmail.com>
2015-06-23 08:16:06 +0000

    Улучшен отчет об использовании тегов

--- TagUsageQuery.cs
+++ TagUsageQuery.cs
@@ -34,8 +34,9 @@ namespace Istra {
 			string pagesPath = contentPath + @"pages/";
 
 			Dictionary<string, Dictionary<string, bool>> tagsRegistry = new Dictionary<string, Dictionary<string, bool>>();
-			CollectTags(contentPath, tagsRegistry);
-			CollectTags(pagesPath, tagsRegistry);
+			CollectTags(contentPath, tagsRegistry, true);
+			//CollectTags(pagesPath, tagsRegistry);
+			CollectFromFile(Istra.SiteSettings.Current.RootDir + @"/users.xml", tagsRegistry);
 
 			StringWriter wrt = new StringWriter();
 			wrt.Write("{");
@@ -48,7 +49,7 @@ namespace Istra {
 				foreach (string file in filesDict.Keys) {
 					if (!filesDict[file]) continue;
 					if (firstFile) firstFile = false; else wrt.Write(";");
-					wrt.Write(file);
+					wrt.Write(JsonUtility.PrepareString(file, true));
 				}
 				wrt.Write(@"""");
 			}
@@ -56,26 +57,51 @@ namespace Istra {
 			requestRoot.InnerText = wrt.ToString();
 		}
 
+		/// <summary>Собирает данные о тегах из заданной директории и ее поддиректорий</summary>
+		/// <param name="dir">сканируемая директория</param>
+		/// <param name="tagsRegistry">реестр найденных тегов</param>
 		private void CollectTags(string dir, Dictionary<string, Dictionary<string, bool>> tagsRegistry) {
+			CollectTags(dir, tagsRegistry, true);
+		}
+
+		/// <summary>Собирает данные о тегах из заданной директории</summary>
+		/// <param name="dir">сканируемая директория</param>
+		/// <param name="tagsRegistry">реестр найденных тегов</param>
+		/// <param name="recursive">признак рекурсивного сканирования поддиректорий</param>
+		private void CollectTags(string dir, Dictionary<string, Dictionary<string, bool>> tagsRegistry, bool recursive) {
 
 			foreach (string file in Directory.GetFiles(dir)) {
 				if (!reXmlFile.IsMatch(file)) continue;
 				try {
-					XmlDocument xDoc = new XmlDocument();
-					xDoc.Load(file);
-					XmlNodeList tags = xDoc.SelectNodes("//*");
-					foreach (XmlNode nd in tags) {
-						if (!(nd is XmlElement)) continue;
-						XmlElement tag = (XmlElement)nd;
-						if (!tagsRegistry.ContainsKey(tag.Name)) tagsRegistry[tag.Name] = new Dictionary<string, bool>();
-						Dictionary<string, bool> docs = tagsRegistry[tag.Name];
-						string relFile = file.Replace(contentPath, string.Empty);
-						docs[relFile] = true;
-					}
+					CollectFromFile(file, tagsRegistry);
 				}
 				catch (Exception) {
 				}
 			}
+			
+			if (!recursive) return;
+
+			foreach (string subDir in Directory.GetDirectories(dir)) {
+				CollectTags(subDir, tagsRegistry);
+			}
 		}
+
+		/// <summary>Собирает данные о тегах из заданного файла</summary>
+		/// <param name="file">сканируемый файл</param>
+		/// <param name="tagsRegistry">реестр найденных тегов</param>
+		private void CollectFromFile(string file, Dictionary<string, Dictionary<string, bool>> tagsRegistry) {
+			XmlDocument xDoc = new XmlDocument();
+			xDoc.Load(file);
+			XmlNodeList tags = xDoc.SelectNodes("//*");
+			foreach (XmlNode nd in tags) {
+				if (!(nd is XmlElement)) continue;
+				XmlElement tag = (XmlElement)nd;
+				if (!tagsRegistry.ContainsKey(tag.Name)) tagsRegistry[tag.Name] = new Dictionary<string, bool>();
+				Dictionary<string, bool> docs = tagsRegistry[tag.Name];
+				string relFile = file.Replace(SiteSettings.Current.RootDir, string.Empty).Replace(@"\", "/");
+				docs[relFile] = true;
+			}
+		}
+
 	}
 }
