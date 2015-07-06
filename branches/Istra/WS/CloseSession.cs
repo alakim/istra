66f3c977482529e682d1d50799d9af385b326978
alvakim@gmail.com <alvakim@gmail.com@b51b9f8c-933b-e06d-08a9-db97f71409cb>
2015-07-06 12:55:26 +0000

    Возможность принудительного закрытия сессии

---
+++ branches/Istra/WS/CloseSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Istra.WS {
+	/// <summary>Принудительно закрывает сессию</summary>
+	public class CloseSession : Istra.WS.WebService {
+
+		protected override void Render(System.Web.UI.HtmlTextWriter writer) {
+			base.Render(writer);
+			if (!CheckSession(writer)) return;
+
+			string sessionID = Request["id"];
+
+			IUserSessionManager manager = SiteSettings.Current.GetSessionManager();
+			manager.Close(sessionID);
+
+			WriteSuccess(writer);
+		}
+	}
+}
--- branches/Istra/WS/SessionsList.cs
+++ branches/Istra/WS/SessionsList.cs
@@ -12,23 +12,28 @@ namespace Istra.WS {
 
 			IUserSessionManager manager = SiteSettings.Current.GetSessionManager();
 
-			List<SessionData> sessions = manager.GetSessions();
-			writer.Write(@"{""sessions"":[");
-			bool first = true;
-			foreach (SessionData data in sessions) {
-				if (first) first = false; else writer.Write(",");
-				writer.Write(
-					@"{{""id"":""{0}"",""userID"":""{1}"",""userName"":""{2}"",""lastAccess"":""{3}""}}",
-					data.SessionID, data.UserID, data.UserName, data.LastAccess
-				);
+			try {
+				List<SessionData> sessions = manager.GetSessions();
+				writer.Write(@"{""sessions"":[");
+				bool first = true;
+				foreach (SessionData data in sessions) {
+					if (first) first = false; else writer.Write(",");
+					writer.Write(
+						@"{{""id"":""{0}"",""userID"":""{1}"",""userName"":""{2}"",""lastAccess"":""{3}""}}",
+						data.SessionID, data.UserID, data.UserName, data.LastAccess
+					);
+				}
+				writer.Write("]");
+
+				writer.Write(@",""time"":""{0}""", JsonUtility.FormatDate(DateTime.Now));
+
+				writer.Write(@",""timeout"":{0}", manager.SessionTimeout);
+
+				writer.Write("}");
 			}
-			writer.Write("]");
-
-			writer.Write(@",""time"":""{0}""", JsonUtility.FormatDate(DateTime.Now));
-
-			writer.Write(@",""timeout"":{0}", manager.SessionTimeout);
-
-			writer.Write("}");
+			catch (Exception err) {
+				WriteError("Ошибка получения списка сессий", err, writer);
+			}
 		}
 	}
 }
